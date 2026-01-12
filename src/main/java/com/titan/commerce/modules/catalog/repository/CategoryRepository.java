0ec20127beb8c4c0a9734fc569507c4a6253cac7
Júlio Vieira <juliovieira035@gmail.com>
2026-01-12 09:47:35 -0300

    Create CategoryRepository.java

---
+++ src/main/java/com/titan/commerce/modules/catalog/repository/CategoryRepository.java
@@ -0,0 +1,19 @@
+package com.titan.commerce.modules.catalog.repository;
+
+import com.titan.commerce.modules.catalog.domain.Category;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+    //busca pelo slug da categoria
+    Optional<Category> findBySlug(String slug);
+
+    //verifica se a slug existe
+    boolean existsBySlug(String slug);
+
+}
+
