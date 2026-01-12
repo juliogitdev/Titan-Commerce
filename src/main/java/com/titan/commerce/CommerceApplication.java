23e7888cb69d8c520cf0cfdc2370e6142929df15
Júlio Vieira <juliovieira035@gmail.com>
2026-01-12 18:34:44 -0300

    Update CommerceApplication.java

--- src/main/java/com/titan/commerce/CommerceApplication.java
+++ src/main/java/com/titan/commerce/CommerceApplication.java
@@ -1,15 +1,20 @@
 package com.titan.commerce;
 
+import com.titan.commerce.modules.catalog.domain.Category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.titan.commerce")
 @EnableJpaAuditing
 public class CommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommerceApplication.class, args);
+
+
 	}
 
 }
