b67f2f6031166ecf4be08f23c03a21aaf5be77ed
Júlio Vieira <juliovieira035@gmail.com>
2026-01-26 10:53:40 -0300

    resolve StackOverflowError on login by adding UserDetailsService
    Added a custom UserDetailsService implementation by overriding loadUserByUsername, enabling Spring Security to properly load users by email during authentication.
    
    This change fixes a critical issue where the login process caused a StackOverflowError due to missing authentication provider resolution, leading to an infinite proxy invocation loop in AuthenticationManager.authenticate().
    
    With this update:
    
    Spring Security authentication flow is correctly resolved
    
    JWT login works as expected
    
    Stack overflow error during login is fully resolved

--- src/main/java/com/titan/commerce/modules/user/domain/User.java
+++ src/main/java/com/titan/commerce/modules/user/domain/User.java
@@ -1,10 +1,7 @@
 package com.titan.commerce.modules.user.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -35,6 +32,8 @@ public class User implements UserDetails {
     private boolean active = true;
 
     @ManyToMany(fetch = FetchType.EAGER)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
